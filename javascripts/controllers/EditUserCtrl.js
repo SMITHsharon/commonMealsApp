c1e56509c21045a35367fbfe62e0022bc7708af8
sharon smith <sharon.e.smith2@gmail.com>
2017-06-11 21:56:12 -0500

    login/register plus update profile working

--- javascripts/controllers/EditUserCtrl.js
+++ javascripts/controllers/EditUserCtrl.js
@@ -1,3 +1,31 @@
 app.controller("EditUserCtrl", function($location, $rootScope, $routeParams, $scope, AuthFactory, UserFactory) {
 
+	$scope.userUpdate = {};
+
+	let getUserInfo = () => {
+		var userInfo = firebase.auth().currentUser;
+			if (userInfo) {
+			  $scope.userUpdate.email = userInfo.email;
+			  $scope.userUpdate.username = userInfo.username;
+			  // $scope.userUpdate.profilePicURL = userInfo.profilePicURL;
+			} else {
+			  // No user is signed in.
+			}
+
+	   	UserFactory.getUser($rootScope.user.uid).then((user) => {
+	      $scope.userUpdate.username = user.username;
+	      $scope.userUpdate.profilePicURL = user.profilePicURL;
+
+	    }).catch();
+	};
+    getUserInfo();
+    
+    
+    $scope.updateUser = () => {
+    	UserFactory.editEmail($scope.userUpdate.email);
+		UserFactory.editUser($rootScope.user, $scope.userUpdate).then((results) => {
+		}).catch((error) => {
+			console.log("update user error: ", error);
+		});
+    };
 });
--- javascripts/factories/UserFactory.js
+++ javascripts/factories/UserFactory.js
@@ -1,3 +1,78 @@
 app.factory("UserFactory", function($http, $q, FIREBASE_CONFIG) {
 
+	let addUser = (authData) => {
+	    return $q((resolve, reject) => {
+	      console.log("consoling auth", authData);
+	      $http.post(`${FIREBASE_CONFIG.databaseURL}/users.json`, 
+	        JSON.stringify({ 
+	          uid: authData.uid,
+	          username: authData.username,
+	          // name: authData.name,
+	          unit: authData.unit,
+	          profilePicURL: authData.imgURL
+	        })
+	      )
+	      .then((storeUserSuccess) => {
+	        resolve(storeUserSuccess);
+	      })
+	      .catch((storeUserError) => {
+	        reject(storeUserError);
+	      });
+	    });
+	};
+
+
+  	let getUser = (userId) =>{
+	    return $q((resolve, reject) => {
+	      $http.get(`${FIREBASE_CONFIG.databaseURL}/users.json?orderBy="uid"&equalTo="${userId}"`)
+	        .then((userObject) => {
+	          let users = [];
+	          Object.keys(userObject.data).forEach((key) => {
+	            users.push(userObject.data[key]);
+	            users[0].id = key;
+	          });
+	          resolve(users[0]);
+	        })
+	        .catch((error) => {
+	          reject(error);
+	        });
+	    });
+	};
+
+
+  	let editEmail = (newEmail) => {
+	    var user = firebase.auth().currentUser;
+	      user.updateEmail(newEmail).then(function() {
+	    }, function(error) {
+	        // An error happened.
+	    });
+	};
+
+
+	let editUser = (id, updatedInfo) => {
+		return $q((resolve, reject) => {
+		  $http.put(`${FIREBASE_CONFIG.databaseURL}/users/${id.id}.json`, JSON.stringify({
+		        imageURL: updatedInfo.profilePicURL,
+		        username: updatedInfo.username,
+		        unit: id.unit,
+		        uid: id.uid,
+		        id: id.id
+		  }))
+		  .then((resultz) => {
+		    resolve(resultz);
+		  })
+		  .catch((error) => {
+		    reject(error);
+		  });
+		});
+	};
+  
+	return {
+		addUser:addUser, 
+		getUser:getUser, 
+		editUser:editUser, 
+		editEmail:editEmail
+	};
+
 });
+
